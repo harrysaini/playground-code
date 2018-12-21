9bd4e0e60f251ab5c8579e9e86b579273ba33346
Harish Saini <harish.saini@gwynniebee.com>
2018-12-21 15:42:10 +0530

    eet tak

--- typescript/with-gulp/src/main.ts
+++ typescript/with-gulp/src/main.ts
@@ -1,2 +1,8 @@
 import {sayHello } from './hello';
-console.log(sayHello("TypeScript"));
+
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    elt.innerText = sayHello(name);
+}
+
+showHello("greeting", "TypeScript");
