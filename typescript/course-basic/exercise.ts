88fa88e5ce170c3b2bfa8032a8ff8484ddf69123
Harish Saini <harish.saini@gwynniebee.com>
2019-01-15 17:10:23 +0530

    fdsfd

--- typescript/course-basic/exercise.ts
+++ typescript/course-basic/exercise.ts
@@ -16,3 +16,34 @@ myself.bankAccount.deposit(3000);
 
 console.log(myself);
 
+//-------------- es6 -------------//
+
+// Exercise 1
+let double = (value: number):number =>  value * 2; ;
+console.log(double(10));
+
+// Exercise 2
+let greet = (name : string = "Max"): void => {
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+
+// Exercise 3
+let numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
+
+// Exercise 4
+let newArray = [55, 20];
+newArray.push(...numbers);
+console.log(newArray);
+
+// Exercise 5
+let testResults = [3.89, 2.99, 1.38];
+let [ result1, result2, result3 ] = testResults
+console.log(result1, result2, result3);
+
+// Exercise 6
+let scientist = {firstName: "Will", experience: 12};
+let { firstName , experience } = scientist;
+console.log(firstName, experience);
